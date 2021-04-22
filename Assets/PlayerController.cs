77f40a4d44c5fe6cc1509108c37ff9c6a248494d
FrogFace <tobyr431@hotmail.co.uk>
2021-04-22 17:37:36 +0100

    Added CharacterController. Added PlayerController.cs
    Added Character controller to player.
    Created and added PlayerController.cs to player

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Config")]
+    [SerializeField]
+    private float movementSpeed = 1f;
+
+
+    //[Header("References")]
+
+    CharacterController charController = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get inputs
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+
+        //create movement vector with movement speed
+        Vector3 movementVector = new Vector3(xInput, 0, yInput) * movementSpeed;
+
+        //update character controller
+        charController.SimpleMove(movementVector);
+    }
+}
