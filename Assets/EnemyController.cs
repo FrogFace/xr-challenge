aff4bc9ed96811cfe212d6cbee94873f17e01daa
FrogFace <tobyr431@hotmail.co.uk>
2021-04-23 23:02:41 +0100

    Added EnemyController.cs,  enemy GameObject and baked Navmesh
    Added EnemyController.cs,
    Added enemy GameObject,
    Baked Navmesh.

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    private Transform player = null;
+    private NavMeshAgent agent = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, player.position) > 2f)
+        {
+            agent.SetDestination(player.position);
+            agent.isStopped = false;
+        }
+        else
+        {
+            agent.isStopped = true;
+        }
+    }
+}
