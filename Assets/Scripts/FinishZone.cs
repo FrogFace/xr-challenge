3665f1fd20feb1ff373f04facfa253da19d50168
FrogFace <tobyr431@hotmail.co.uk>
2021-04-28 00:38:28 +0100

    Added Game Manager, changed ScoreSystem to UIManager
    Added GameManager.cs,
    Changed ScoreSystem.cs to UIManager.cs,
    Changed Score and Star management logic to GameManager
    changed FinishZone Script exectuion order to default.
    changed GameManager.cs Script Execution order to run after stars initialize

--- Assets/Scripts/FinishZone.cs
+++ Assets/Scripts/FinishZone.cs
@@ -9,39 +9,21 @@ public class FinishZone : MonoBehaviour
     private Material closedMaterial = null;
     [SerializeField]
     private Material openMaterial = null;
-    [SerializeField]
-    private Pickup[] pickupArray;
 
     private MeshRenderer meshRenderer = null;
     private bool isOpen = false;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         //get the mesh render and set to closed material
         meshRenderer = GetComponent<MeshRenderer>();
         meshRenderer.material = closedMaterial;
-
-        //subscribe to all of the star pickup events
-        foreach (Pickup star in pickupArray) star.OnPickUp += UnlockCheck;
     }
 
-    /// <summary>
-    /// Check if all stars have been collected.
-    /// Unlocks door if all have been collected.
-    /// </summary>
-    /// <param name="obj"></param>
-    private void UnlockCheck(Pickup obj)
+    public void UnlockExit()
     {
-        //loop through all stars and count each collected
-        int totalCollected = 0;
-        for (int i = 0; i < pickupArray.Length; i++)
-        {
-            if (pickupArray[i].IsCollected) totalCollected++;
-        }
-
-        //if all stars are collected open door
-        if (totalCollected == pickupArray.Length) isOpen = true;
+        isOpen = true;
 
         //update material to open
         meshRenderer.material = isOpen ? openMaterial : closedMaterial;
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text scoreText = null;
+    [SerializeField]
+    private Text starText = null;
+    [SerializeField]
+    private Slider HealthBar = null;
+    [SerializeField]
+    private GameManager gameManager = null; 
+
+    public void UpdateScoreText(int currentScore)
+    {
+        scoreText.text = $"Gold: {currentScore}";
+    }
+
+    public void UpdateHealthBar(float healthPercentage)
+    {
+        HealthBar.value = healthPercentage;
+    }
+
+    public void UpdateStarText(int currentStars, int totalStars)
+    {
+        starText.text = $"Stars: {currentStars} / {totalStars}";
+    }
+}
