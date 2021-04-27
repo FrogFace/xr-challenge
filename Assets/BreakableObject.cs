d7e0b0360afb36039ae79a8171cec80935e9e9bc
FrogFace <tobyr431@hotmail.co.uk>
2021-04-27 01:08:49 +0100

    Added Breakable Barrel, Added BreakableObject.cs
    Added BreakableObject.cs.
    Added BreakableBarrel prefab.
    Added Breakable Tag

---
+++ Assets/BreakableObject.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class BreakableObject : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField]
+    private GameObject normalModel = null;
+    [SerializeField]
+    private GameObject damagedModel = null;
+    [SerializeField]
+    private GameObject destroyedModel = null;
+    [SerializeField]
+    private int currentHealth = 40;
+
+    /// <summary>
+    /// Change the current health value of this item 
+    /// </summary>
+    /// <param name="changeValue">The ammount of health removed</param>
+    public void ModifyHealth(int changeValue)
+    {
+        //reduce current health by value, clamp between 0 and maxHealth
+        currentHealth -= changeValue;
+
+        if(currentHealth > 0)
+        {
+            normalModel.SetActive(false);
+            damagedModel.SetActive(true);
+        }
+        else
+        {
+            damagedModel.SetActive(false);
+            destroyedModel.SetActive(true);
+            GetComponent<Collider>().enabled = false;
+        }
+    }
+}
