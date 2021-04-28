f131d7f3d7d1e97d6a1a3c3cf45a9c4e823c4d6c
FrogFace <tobyr431@hotmail.co.uk>
2021-04-28 09:57:12 +0100

    Added controller support to menus

--- Assets/MainMenuManager.cs
+++ Assets/MainMenuManager.cs
@@ -1,7 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class MainMenuManager : MonoBehaviour
 {
@@ -10,6 +9,14 @@ public class MainMenuManager : MonoBehaviour
     private GameObject mainMenu = null;
     [SerializeField]
     private GameObject guideUI = null;
+    [SerializeField]
+    private GameObject aboutUI = null;
+    [SerializeField]
+    private GameObject mainMenuStartingButton = null;
+    [SerializeField]
+    private GameObject aboutMenuStartingButton = null;
+    [SerializeField]
+    private GameObject guideMenuStartingButton = null;
 
 
     // Start is called before the first frame update
@@ -17,21 +24,43 @@ public class MainMenuManager : MonoBehaviour
     {
         Time.timeScale = 1;
     }
+    public void EnableMainMenu()
+    {
+        mainMenu.SetActive(true);
+        guideUI.SetActive(false);
+        aboutUI.SetActive(false);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(mainMenuStartingButton);
+    }
 
     public void EnableGuideMenu()
     {
         mainMenu.SetActive(false);
         guideUI.SetActive(true);
+        aboutUI.SetActive(false);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(guideMenuStartingButton);
     }
 
-    public void EnableMainMenu()
+    public void EnableAboutMenu()
     {
-        mainMenu.SetActive(true);
+        mainMenu.SetActive(false);
         guideUI.SetActive(false);
+        aboutUI.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(aboutMenuStartingButton);
     }
 
     public void PlayGame()
     {
         SceneManager.LoadScene("Main");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
